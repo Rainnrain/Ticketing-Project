7b3fe375d266615232f0f8d43a5e82f915d26d4f
Lorraine <lorrainecannom@gmail.com>
2022-11-01 15:09:47 -0400

    Connecting to database

--- src/main/java/com/cydeo/repository/RoleRepository.java
+++ src/main/java/com/cydeo/repository/RoleRepository.java
@@ -3,8 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository <Role, Long> {
     // give the role based on the description
 
     Role findByDescription(String description); //Derived method query
+
+
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,14 +1,31 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+
+    }
+
     @Override
     public List<RoleDTO> listAllRoles() {
+        List <Role> roleList= roleRepository.findAll();
+
+
+
+
         return null;
     }
 
